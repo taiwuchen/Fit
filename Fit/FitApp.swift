e09e367583cfd77ab4fbe01ed72acdd3d4349782
Taiwu <chen.taiwu@ucalgary.ca>
2024-07-17 09:20:28 +0800

    Initial Commit

---
+++ Fit/FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitApp.swift
+//  Fit
+//
+//  Created by Taiwu Chen on 2024-07-17.
+//
+
+import SwiftUI
+
+@main
+struct FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
